7fe4445d9a025125fac048139d3f62284a034a27
Patrick <patrickpellicciotta@gmail.com>
2022-02-19 00:42:18 -0500

    Added Quiz.02.py

---
+++ Class01/Quiz.02.py
@@ -0,0 +1,22 @@
+'''
+QUIZ #2
+Name: Patrick Pellicciotta
+ID: 2210211
+
+Follow the instructions in the comments below. Then upload this file, with your answers, to Omnivox.
+'''
+# 1. Create a list that contains at least three strings
+a = ['no', 'now', 'never']
+
+# 2. Print the last string in the list
+print(a[-1])
+
+# 3. Using the len() function, print the length of the list
+print(len(a))
+
+# 4. Write a logical statement that uses the or operator and evaluates to true
+b = False or True
+
+# 5. Write an if statement without an else
+if a:
+    print('congrats')
